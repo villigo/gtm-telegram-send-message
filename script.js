2505735ea2c5178feaf9045a071b06f9abdabf89
villigo <40770913+villigo@users.noreply.github.com>
2021-10-30 11:13:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
     var message = '<b>Форма:</b> ' + formName + '%0A' + '<b>Имя:</b> ' + name + '%0A' + '<b>Телефон:</b> ' + phone + '%0A' + '<b>Сообщение:</b> ' + text;
     
     var chat_id = <id>; //id получателя или группы
-    var token = <token>;  //Ваш токен
+    var token = <token>;  //Ваш токен. Указываем в кавычках
     
     //Тут лучше ничего не трогать
     var url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text='+message+'&parse_mode=html';
